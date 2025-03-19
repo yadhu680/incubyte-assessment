4e6686058c54be39f48f656e3db1f210888a8ffa
Yadhunadh Chemmarassery <yadhu680@gmail.com>
2025-03-19 12:57:35 +0000

    Adding functionality for 1,2,3 or 1\n2\n3 or 1\n2, 3 checking

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Test Calculator functionality here:
 function stringCalculatorTester() {
     const inpStr = document.getElementById('inputString').value;
-    document.getElementById('result').textContent = checkResult(inpStr, 0);
+    document.getElementById('result').textContent = checkResult(inpStr, 6);
     function checkResult(val, res) {
       return (result =
         strCalculator.calculate(val) === res
@@ -13,9 +13,22 @@ function stringCalculatorTester() {
 // Function to Calculate based on entered numbers
 const strCalculator = {
     calculate: function (numbers) {
-        if (numbers === '""') return 0;      
-    }
-};
+      if (numbers === '""') return 0;
+  
+      numbers = numbers.replaceAll('\\', '');
+  
+      let delimiter = /[\\n,]/; // Default delimiters: comma or newline      
+  
+      const splitNumbers = numbers.split(delimiter);
+      let sum = 0;      
+      for (let i = 0; i < splitNumbers.length; i++) {        
+          sum += parseInt(splitNumbers[i]);        
+      }
+      numbers = sum;
+      return numbers;
+        
+    },
+  };
 
 // Calling the calculate function here
 const btn = document.querySelector('#calculate');
