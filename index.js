fcd7b377560088451b5ea1ace2e96f3330ee624b
Yadhunadh Chemmarassery <yadhu680@gmail.com>
2025-03-19 12:50:23 +0000

    Checking input as ""

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Test Calculator functionality here:
 function stringCalculatorTester() {
     const inpStr = document.getElementById('inputString').value;
-    document.getElementById('result').textContent = checkResult(inpStr, 6);
+    document.getElementById('result').textContent = checkResult(inpStr, 0);
     function checkResult(val, res) {
       return (result =
         strCalculator.calculate(val) === res
@@ -9,3 +9,10 @@ function stringCalculatorTester() {
           : `Failed: Evaluating ${val} to ${res}`);
     }
 }
+
+// Function to Calculate based on entered numbers
+const strCalculator = {
+    calculate: function (numbers) {
+        if (numbers === '""') return 0;      
+    }
+};
