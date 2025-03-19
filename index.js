25b613c803fa5266da8926528fd04472186ebb9b
Yadhunadh Chemmarassery <yadhu680@gmail.com>
2025-03-19 13:02:06 +0000

    Adding negative number exception functionality

--- index.js
+++ index.js
@@ -13,19 +13,34 @@ function stringCalculatorTester() {
 // Function to Calculate based on entered numbers
 const strCalculator = {
     calculate: function (numbers) {
-      if (numbers === '""') return 0;
-  
-      numbers = numbers.replaceAll('\\', '');
-  
-      let delimiter = /[\\n,]/; // Default delimiters: comma or newline      
-  
-      const splitNumbers = numbers.split(delimiter);
-      let sum = 0;      
-      for (let i = 0; i < splitNumbers.length; i++) {        
-          sum += parseInt(splitNumbers[i]);        
-      }
-      numbers = sum;
-      return numbers;
+        if (numbers === '""') return 0;
+    
+        numbers = numbers.replaceAll('\\', '');
+    
+        let delimiter = /[\\n,]/; // Default delimiters: comma or newline      
+    
+        const splitNumbers = numbers.split(delimiter);
+        let sum = 0;
+        const negativeNumbers = [];
+        
+        // Check for negative numbers else continue adding
+        for (let i = 0; i < splitNumbers.length; i++) {
+            if (splitNumbers[i] < 0) {
+                negativeNumbers.push(splitNumbers[i]);
+            } else {
+                sum += parseInt(splitNumbers[i]);
+            }
+        }
+
+        numbers = sum;
+
+        // Display an exception for negative numbers
+        negativeNumbers.length > 0
+        ? (document.getElementById('error').textContent =
+            'Negative numbers not allowed ' + negativeNumbers.join(', '))
+        : (document.getElementById('error').textContent = '');
+
+        return numbers;
         
     },
   };
